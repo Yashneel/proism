418a65fd63f2d2573e3ccf95b545a22b5c45bea5
Yashneel <79652274+Yashneel@users.noreply.github.com>
2021-07-16 23:32:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-function setup(){
+cocosSd = ""
 
+function setup(){
+canvas = createCanvas(300,300)
+canvas.center()
+	video = createCapture(VIDEO);
+	video.hide();
 }
 function preload(){
 
 }
 function draw(){
+    image(video, 0, 0, 300, 300)
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded())
     
+}
+function modelLoaded(){
+	console.log("Model loaded omg it ate cat")
 }
